a7cc2096ab6f59392e4da6ebe7cf73612a9e2d71
Dan <daniel.munozdev@gmail.com>
2017-10-21 18:11:54 -0700

    Add variables with values from form inputs, and add them to spans in info div

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 $('form#survey').submit(function(event) {
   event.preventDefault();
-
+  let name = $('input#name').val();
+  let beverage = $('select#beverage').val();
+  let flavor = $('input#flavor:checked').val();
+  // let flavor = $('input:radio[name=flavor]:checked').val();
+  let dob = $('input#born').val();
+  let color = $('input#color').val();
+  $('span#outputInput').text(name);
+  $('span#outputSelect').text(beverage);
+  $('span#outputRadio').text(flavor);
+  $('span#outputDob').text(dob);
+  $('span#outputColor').text(color);
   $('div.info').slideDown();
 });
