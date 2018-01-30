1be96c4a9960bba58a9dec0268281def599e8694
Daniel Munoz <daniel.munozdev@gmail.com>
2018-01-30 12:44:31 -0800

    Add comments, focus event listener, and a for loop to minimize repetition

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
 // Business (or back-end) logic:
+// a function to fill in a span with the value of an input
 function inputReplaceText (selector, value) {
  return $(selector).text(value);
 }
 
 //Everything below this line is user interface (or front-end) logic:
-$('form#survey').submit(function(event) {
+// form submit event listener
+$('form#survey').on('submit', function(event) {
+  // prevent default form submit action
   event.preventDefault();
+
+  // store inputs into variables
   let name = $('input#name').val();
   let beverage = $('select#beverage').val();
-  let flavor = $('input#flavor:checked').val();
-  // let flavor = $('input:radio[name=flavor]:checked').val();
+  let flavor = $('input:radio[name=flavor]:checked').val();
   let dob = $('input#born').val();
   let color = $('input#color').val();
-  inputReplaceText('span#outputInput', name);
-  inputReplaceText('span#outputSelect', beverage);
-  inputReplaceText('span#outputRadio', flavor);
-  inputReplaceText('span#outputDob', dob);
-  inputReplaceText('span#outputColor', color);
+
+  // array of inputs and span selectors
+  let inputs = ['span#outputInput', 'span#outputSelect', 'span#outputRadio', 'span#outputDob', 'span#outputColor'];
+  let selectors = [name, beverage, flavor, dob, color];
+
+  // for loop to fill in empty spans with input data
+  for (let i = 0; i < selectors.length; i++) {
+    inputReplaceText(inputs[i], selectors[i]);
+  }
+  // show hidden div with input data
   $('div.info').slideDown();
+});
+
+// input, and select focus event listener
+$('input, select').on('focus', function() {
+  // hide div.info when an input or select is focused
+  $('div.info').slideUp();
 });
